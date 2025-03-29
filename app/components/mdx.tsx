6ab67b74fc41fcadd49adc9785e359478da883b3
motiveko <rhehdrla@naver.com>
2025-03-29 15:48:05 +0900

    mdx 개선

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -7,16 +7,29 @@ import React from "react";
 
 function Code({ children, className, ...props }) {
   // className이 없는 경우는 인라인 코드를 의미
-  const isInline = !className;
+  const isInline = typeof children === "string";
+  if (isInline) {
+    return (
+      <code className={isInline ? "inline-code" : className} {...props}>
+        {children}
+      </code>
+    );
+  }
+
+  return children;
+}
+
+function Blockquote({ children }) {
   return (
-    <code className={isInline ? "inline-code" : className} {...props}>
+    <blockquote className="border-l-4 border-gray-300 dark:border-gray-700 pl-4 my-4 italic text-gray-700 dark:text-gray-300">
       {children}
-    </code>
+    </blockquote>
   );
 }
 
 const components = {
   code: Code,
+  blockquote: Blockquote,
 };
 
 export function CustomMDX(props) {
